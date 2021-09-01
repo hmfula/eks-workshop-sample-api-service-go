977c32a0b6abc9e5d688b2ab373a68e2d76c97bd
hmfula <hmfula@yahoo.com>
2021-09-01 13:02:57 +0300

    Update main.go
    Small change

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Tech Demo 1 2 !:) "}
+		res := &response{Message: "Hello Tech Demo 1 2 3!:) "}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
