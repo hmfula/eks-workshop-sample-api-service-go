60be285d31d77a602238ce4fdf1fe5c1b31ab8ae
hmfula <hmfula@yahoo.com>
2021-09-01 11:52:11 +0300

    Update main.go
    trigger build

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Tech Demo 1 2 3:) "}
+		res := &response{Message: "Hello Tech Demo 1 2 3 Go !:) "}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
