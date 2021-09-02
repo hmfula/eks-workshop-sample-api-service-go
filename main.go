3dfcfc1894eee74ffdbf2b557233adbdf88e6709
hmfula <hmfula@yahoo.com>
2021-09-02 09:12:28 +0300

    Update main.go
    updated code

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Tech Demo - AWS CodePipeline  is working!:) "}
+		res := &response{Message: "Hello Tech Demo - AWS CI/CD Pipeline  is working!!:) "}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
